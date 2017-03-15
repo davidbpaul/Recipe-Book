db14dc47fe7b2a59a72526cdb8313ce373dcce36
David Paul <david-paul23@hotmail.com>
2017-03-14 23:35:49 -0400

    ionic practice. before side bar features

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
+import { RecipesPage } from '../pages/recipes/recipes';
+import { RecipePage } from '../pages/recipe/recipe';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ShoppingListService } from '../services/shopping-list';
+import { RecipesService} from '../services/recipes'
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    EditRecipePage,
+    RecipesPage,
+    RecipePage,
+    ShoppingListPage,
+    TabsPage,
+
+
+
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    EditRecipePage,
+    RecipesPage,
+    RecipePage,
+    ShoppingListPage,
+    TabsPage,
+
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService,
+    RecipesService
+  ]
+})
+export class AppModule {}
